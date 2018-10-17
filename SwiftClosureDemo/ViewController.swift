033387822d4af48e60b1c0c68f389eb22b1395a2
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-10-17 01:54:05 -0700

    sample demo with closure

--- SwiftClosureDemo/ViewController.swift
+++ SwiftClosureDemo/ViewController.swift
@@ -13,8 +13,84 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        
+       let  result = addTwoNumbers(x: 10, y: 20)
+    
+    print(result)
+
+
+    
     }
 
+//// this is normal
+
+    func addTwoNumbers(x:Int,y:Int) -> Int{
+        
+        
+        return x + y
+    }
+    override func viewWillAppear(_ animated: Bool) {
+    
+//// closure having no func keyword and no name ///
+        
+        
+        var sumClosure:(Int,Int) -> Int = { x,y in
+            
+            
+            return x + y
+        }
+        
+
+   let resultClosure = sumClosure(20,20)
+    
+    print("sum by using closures",resultClosure)
+    }
+
+
+    /// one more way  ///
+    
+    override func viewDidAppear(_ animated: Bool) {
+    
+    
+    let name = hello()
+    
+    print(name)
+    
+    
+        //// closure call
+    
+    
+  
+    
+    //// 2ns way closure call
+        
+   let nameDetails =     helloWithClosure()
+        
+        
+        print(nameDetails)
+    }
+  
+    
+    func hello() -> String {
+        
+       return "raghu"
+        
+    }
+    
+    
+//// 2nd way using closures ///
+    
+    
+    var  helloWithClosure:() -> String  = {
+        
+        return "my name is raghu"
+        
+    }
+    
+
+    
 
 }
 
